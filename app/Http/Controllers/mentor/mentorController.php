b1d60597480d816a5f5ffdf226dca712704997c9
narunbabu <narun.iitb@gmail.com>
2018-08-01 20:19:07 +0000

    task edit error recctified

--- app/Http/Controllers/mentor/mentorController.php
+++ app/Http/Controllers/mentor/mentorController.php
@@ -608,7 +608,7 @@ class mentorController extends Controller
         $task->update();
 
         $chapters = coursetask::where('task_id',$task->id)
-        ->join('chapter','chapter.id','coursetasks.chapter_id')->select('course_id')
+        ->join('chapters','chapters.id','coursetasks.chapter_id')->select('course_id')
         ->get();
         foreach($chapters as $chapter){
             self::UpdatetotalCoursecredits($chapter ->course_id);
